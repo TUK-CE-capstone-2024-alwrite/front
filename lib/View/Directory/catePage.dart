f49ba6c44637b52fadc3c5d3d071b8e70c58ab29
ohamin26 <ohamin26@gmail.com>
2024-05-15 17:57:16 +0900

    fix : ocr 버튼 수정

--- lib/View/Directory/catePage.dart
+++ lib/View/Directory/catePage.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:alwrite/Controller/canvasController.dart';
-import 'package:alwrite/View/drawingPage.dart';
 
 class CategoryPage extends StatelessWidget {
   final String category;
